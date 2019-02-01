5e38b48ebbe0244c3d8bda611abd8224a9f91a9d
Michael Grupp <grupp@magazino.eu>
2019-02-01 22:06:43 +0100

    Implement PropertyClient class.

--- api_test.py
+++ api_test.py
@@ -1,6 +1,12 @@
 from __future__ import print_function
 
+try:
+    import thread
+except ImportError:
+    import _thread as thread
 import json
+import time
+import pprint
 
 # pip install websocket-client
 # !! NOT: pip install websocket
@@ -8,16 +14,49 @@ import websocket
 
 URL = "ws://schuecobe5hackdays.azurewebsites.net/WebSocketServer.ashx?"
 
+CONNECTION_REQUEST = {
+    "type": "connection_request",
+    "request_type": "connect_to_prop",
+    "prop_id": 0
+}
+
+
+class PropertyClient(object):
+    def __init__(self, prop_id=0):
+        self.prop_id = prop_id
+        self.data = {}
+        websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp(
+            URL,
+            on_open=lambda ws: self.on_open(ws),
+            on_message=lambda ws, msg: self.on_message(ws, msg),
+            on_error=lambda ws, msg: self.on_error(ws, msg),
+            on_close=lambda ws: self.on_close(ws))
+
+    def on_message(self, ws, message):
+        payload = json.loads(message)
+        if payload["type"] == "property_update":
+            self.data.update({payload["value_name"]: payload["value"]})
+            print(
+                "Received an update, full data is now:",
+                pprint.pformat(self.data),
+                sep="\n")
+
+    def on_error(self, ws, error):
+        print("ERROR:", error)
+
+    def on_close(self, ws):
+        print("Socket closed!")
+
+    def on_open(self, ws):
+        def run(*args):
+            connection_request = CONNECTION_REQUEST
+            connection_request["prop_id"] = self.prop_id
+            self.ws.send(json.dumps(CONNECTION_REQUEST))
+
+        thread.start_new_thread(run, ())
+
+
 if __name__ == '__main__':
-    ws = websocket.WebSocket()
-    ws.connect(URL)
-    data = {
-        "type": "connection_request",
-        "request_type": "connect_to_prop",
-        "prop_id": 0
-    }
-    ws.send(json.dumps(data))
-    response = ws.recv()
-    print(response)
-    data = json.loads(response)
-    print("I received value: ", data["value"])
+    prop = PropertyClient(prop_id=0)
+    prop.ws.run_forever()
